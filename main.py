c47a07b7c64468124fdab2bf838faad183aecfa2
markkong30 <markkong30@gmail.com>
2023-05-31 22:46:13 +0100

    initial setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import time
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://github.com/")
