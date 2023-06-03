7808a3d28b64bf57fa3c3092335d83fb56b6b2bd
markkong30 <markkong30@gmail.com>
2023-06-04 00:19:40 +0100

    add enum constants

---
+++ booking/constants.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Smoking_type(Enum):
+    NON_SMOKING = 0
+    SMOKING = 1
+
+
+class No_of_people(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+
+
+class Room_type(Enum):
+    SINGLE = 10
+    DOUBLE = 20
+    TWIN = 30
--- booking/reserve_picker.py
+++ booking/reserve_picker.py
@@ -2,6 +2,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from constants import Smoking_type, No_of_people, Room_type
 
 
 def reserve_picker(driver: WebDriver):
@@ -10,13 +11,13 @@ def reserve_picker(driver: WebDriver):
     date_picker.send_keys("20231102", Keys.ENTER)
 
     number_of_people = Select(driver.find_element(By.NAME, "rmnd_ppl"))
-    number_of_people.select_by_value("2")
+    number_of_people.select_by_value(No_of_people.TWO)
 
     smoking_type = Select(driver.find_element(By.NAME, "smkng_type"))
-    smoking_type.select_by_value("0")
+    smoking_type.select_by_value(Smoking_type.NON_SMOKING)
 
     room_type = Select(driver.find_element(By.NAME, "room_type"))
-    room_type.select_by_value("20")
+    room_type.select_by_value(Room_type.DOUBLE)
 
     driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
 
